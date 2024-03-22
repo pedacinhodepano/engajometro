33678bc3bcf48bf5517c36fc2cf6d47a48c80bec
leandro-de-paula <leandrodepaula.ti@gmailcom>
2024-03-22 13:27:33 -0300

    Debug - Bug in home page - 04

--- pages/page.js
+++ pages/page.js
@@ -1,6 +1,5 @@
 // Links Header
 document.getElementById('index').href = PAGE ? '../index.html' : 'index.html'
-document.getElementById('logo').src = PAGE ? '../assets/img/logo.png' : 'assets/img/logo.png'
 document.getElementById('home').href = PAGE ? '../index.html' : 'index.html'
 
 
